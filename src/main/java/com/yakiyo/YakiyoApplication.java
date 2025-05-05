26d47be456e6b64777c7a48948d17db0f9dd1cef
kohj-git <22000071@handong.ac.kr>
2025-05-05 23:38:14 +0900

    Initial commit

---
+++ src/main/java/com/yakiyo/YakiyoApplication.java
@@ -0,0 +1,13 @@
+package com.yakiyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakiyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakiyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yakiyo/YakiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakiyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakiyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
